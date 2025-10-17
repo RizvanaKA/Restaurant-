1da991152d6d4b90c80d8ccd2ffe9c645ed2264f
RizvanaKA <richuu964@gmail.com>
2025-10-17 15:49:07 +0530

    Add files via upload

--- Restuarant/settings.py
+++ Restuarant/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Restuarant.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'resto3',
+        'NAME': 'resto',
         'HOST': 'localhost',
         'PORT': '3306',
         'USER': 'root',
